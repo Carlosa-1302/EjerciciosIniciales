699232849f6f1a24a7afc11f80ade2b8335b73a7
Carlosa-1302 <carlosandresinfo13@gmail.com>
2024-09-12 19:22:37 +0200

    asda

--- Assets/Ejercicios/Ejercicio1_1.cs
+++ Assets/Ejercicios/Ejercicio1_1.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Ejercicio1_1 : MonoBehaviour
 {
+    string Carlos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("¡Hola Mundo!");
+        Debug.Log("Este es el primer videojuego de Carlos");
+        Debug.Log("Estoy aprendiendo C#");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Ha pasado un frame");
     }
+    //cuando es un void update se actualiza por cada frame el codigo repitiendo el mismo comando
+    //pero en cambio el void start solo lo hace al comienzo (start)
 }
---
+++ Assets/Ejercicios/Ejercicio2_1.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2_1 : MonoBehaviour
+{
+    //VARIABLES GLOBLALES si pones aqui variables lo todo el script
+
+
+
+
+    //[SerializeField] int vida;
+    //el SerializerField es para poder "visualizarlo" afuera por Unity sin que este expuesto a otros scripst
+    void Start()
+    {
+        int edad;
+        float altura;
+        string nombre;
+        float iva;
+        float peso;
+        bool alumnoRepetidor;
+        char letra;
+        int minutos;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
