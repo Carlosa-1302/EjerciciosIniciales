8ad199a037b00e7fb380ef8fff4a11d2adf5d1d8
Carlosa-1302 <carlosandresinfo13@gmail.com>
2024-09-16 21:11:00 +0200

    nuevos ejercicios

---
+++ Assets/Ejercicios/ejemplos clase.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class ejemplosclase : MonoBehaviour
+{
+    [SerializeField] int nivel ;
+    [SerializeField] int vida ;
+    [SerializeField] int edad ;
+    [SerializeField] string nombre ;
+    //[SerializeField] string nombre ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*nivel = 5;
+
+        if (nivel == 16)  //Condicional
+        {
+            Debug.Log("evoluciono");
+        }
+        if (nivel ¡= 100)
+        {
+            Debug.Log("aun me queda niveles");
+        }
+        if(vida <=0)
+        {
+            Debug.Log("has muerto");
+        }
+        if(edad >= 18)
+        {
+            Debug.Log("puedo entrar a la discoteca");
+        }
+        if(edad >= 18)
+        {
+            Debug.Log("puedo conducir legamente");
+        }
+        */
+
+
+        //if
+        //if else
+        // if- else if
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
