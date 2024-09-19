b69b6a016e2d9f41630f64393de2c8536d2011ba
Carlosa-1302 <carlosandresinfo13@gmail.com>
2024-09-19 22:49:01 +0200

    sada

--- Assets/Ejercicios/Ejercicio3_4.cs
+++ Assets/Ejercicios/Ejercicio3_4.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Ejercicio3_4 : MonoBehaviour
 {
+    [SerializeField] int exp;
+    [SerializeField] int LVL = 50;
     // Start is called before the first frame update
     void Start()
     {
-        
+        calcularNivel();
     }
 
     // Update is called once per frame
-    void Update()
+    void calcularNivel()
     {
-        
+       int resultadoLVL = LVL + (9*exp/5);
+        Debug.Log(resultadoLVL);
     }
 }
