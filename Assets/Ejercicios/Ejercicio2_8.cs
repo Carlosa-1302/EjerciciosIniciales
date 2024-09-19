87f82a8c7fabb11356fb7e85711736cb4659e950
Carlosa-1302 <carlosandresinfo13@gmail.com>
2024-09-19 19:20:57 +0200

    fghfgh

--- Assets/Ejercicios/Ejercicio2_8.cs
+++ Assets/Ejercicios/Ejercicio2_8.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class Ejercicio2_8 : MonoBehaviour
 {
-    int vida = 120;
+    float vida = 120;
 
+    float vidaActual;
     // Start is called before the first frame update
     void Start()
     {
-        
+        vidaActual = vida;
+        Debug.Log(vidaActual);
     }
 
     // Update is called once per frame
     void turnos(int vida)
     {
+        float cantidadARestar = vida * 0.03f;
         
+        cantidadARestar = vida * 0.06f;
+        cantidadARestar = vida * 0.09f;
+        cantidadARestar = vida * 0.12f;
+        cantidadARestar = vida * 0.15f;
     }
 }
