8c593a9109fb51b403440a7e5dc9660d8c82027c
Carlosa-1302 <carlosandresinfo13@gmail.com>
2024-09-19 23:53:11 +0200

    dfasd

--- Assets/Ejercicios/Ejercicio3_7.cs
+++ Assets/Ejercicios/Ejercicio3_7.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Ejercicio3_7 : MonoBehaviour
 {
+    [SerializeField] float Base;
+    [SerializeField] float Altura;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        calcularArea();
     }
 
     // Update is called once per frame
-    void Update()
+    void calcularArea()
     {
-        
+        float resultadoArea = (Base * Altura) / 2f;
+        Debug.Log("el area del triangulo es " + resultadoArea);
     }
 }
--- Assets/Ejercicios/Ejercicio4_1.cs
+++ Assets/Ejercicios/Ejercicio4_1.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Ejercicio4_1 : MonoBehaviour
 {
+    [SerializeField] float radio;
+    [SerializeField] float pi = 3.14f;
+    [SerializeField] float Base;
+    [SerializeField] float altura;
+    [SerializeField] float lado;
+    [SerializeField] float lado2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        float areaCirculo = calcularCirculo(5);
+        Debug.Log("el area del ciruclo es " + areaCirculo);
+
+        float areaTriangulo = calcularTriangulo(6, 8);
+        Debug.Log("el aread del triangulo es " + areaTriangulo);
+
+        float areaCuadrado = calcularCuadrado(4, 8);
+        Debug.Log("el area del cuadrado es " + areaCuadrado);
     }
 
     // Update is called once per frame
-    void Update()
+    float calcularCirculo(float radio)
     {
-        
+        float resultadoCirculo = radio*radio * pi;
+        return resultadoCirculo;
+
+    }
+    float calcularTriangulo(float Base, float altura)
+    {
+        float resultadoTriangulo = (Base * altura) / 2;
+        return resultadoTriangulo;
+    }
+    float calcularCuadrado(float lado, float lado2)
+    {
+        float resultadoCuadrado = lado * lado2;
+        return resultadoCuadrado;
     }
 }
