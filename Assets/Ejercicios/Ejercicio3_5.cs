f7f975c504dadc7b788a916f825041e906fd7b2b
Carlosa-1302 <carlosandresinfo13@gmail.com>
2024-09-19 23:05:54 +0200

    fdg

--- Assets/Ejercicios/Ejercicio3_5.cs
+++ Assets/Ejercicios/Ejercicio3_5.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Ejercicio3_5 : MonoBehaviour
 {
+    float Pi = 3.14f;
+    [SerializeField] float radio;
     // Start is called before the first frame update
     void Start()
     {
-        
+        calcularLongitud();
+        areaCircunferencia();
     }
 
     // Update is called once per frame
-    void Update()
+    void calcularLongitud()
     {
-        
+        float resultadoLongitud = 2 * Pi * radio;
+        Debug.Log("la longuitud es " + resultadoLongitud);
+    }
+    void areaCircunferencia()
+    {
+        float recultadoArea = (Pi * radio) * 2;
+        Debug.Log("el area es " + recultadoArea);
     }
 }
