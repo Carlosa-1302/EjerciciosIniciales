7cb256736c14f02aef533d028424b498c8ae4c87
Carlosa-1302 <carlosandresinfo13@gmail.com>
2024-09-19 14:29:52 +0200

    sadasd

--- Assets/Ejercicios/Ejercicio2_7.cs
+++ Assets/Ejercicios/Ejercicio2_7.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Ejercicio2_7 : MonoBehaviour
 {
+    int oro = 7;
+    int segundos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        calcularFarmeo(oro, 14400);
     }
 
     // Update is called once per frame
-    void Update()
+    void calcularFarmeo(int oro, int segundos)
     {
-        
+        int resultadoFarmeo = oro * segundos;
+        Debug.Log(resultadoFarmeo);
+
     }
 }
