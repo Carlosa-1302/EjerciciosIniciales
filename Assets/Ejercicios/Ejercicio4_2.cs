968462f14e10a96731f071bcdb380e36f7bd6621
Carlosa-1302 <carlosandresinfo13@gmail.com>
2024-09-19 23:59:30 +0200

    Update Ejercicio4_2.cs

--- Assets/Ejercicios/Ejercicio4_2.cs
+++ Assets/Ejercicios/Ejercicio4_2.cs
@@ -11,12 +11,12 @@ public class Ejercicio4_2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        construirNombre(carlos, andres, ordoñez, 25)
     }
 
     // Update is called once per frame
     void construirNombre(string nombre, string apellido1, string apellido2, int edad)
     {
-      return Debug.Log("esta perosan se llama" +  nombre + apellido1+ apellido2 + " y tiene " + edad);  
+       Debug.Log("esta perosan se llama" +  nombre + apellido1+ apellido2 + " y tiene " + edad + " años");  
     }
 }
