1fe71f0bb9ee6c2c139155f4e710bc1a0b9721eb
Carlosa-1302 <carlosandresinfo13@gmail.com>
2024-09-19 14:21:04 +0200

    das

--- Assets/Ejercicios/Ejercicio2_6.cs
+++ Assets/Ejercicios/Ejercicio2_6.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Ejercicio2_6 : MonoBehaviour
 {
+    int puntuacion1 = 4;
+    int puntuacion2 = 6;
+    int puntuacion3 = 8;
     // Start is called before the first frame update
     void Start()
     {
+        puntuacionesyMedia();
         
     }
 
     // Update is called once per frame
-    void Update()
+    void puntuacionesyMedia()
     {
-        
+        int puntuacionMedia = (puntuacion1 + puntuacion2 + puntuacion3) / 3;
+        Debug.Log(puntuacion1);
+        Debug.Log(puntuacion2);
+        Debug.Log(puntuacion3);
+        Debug.Log(puntuacionMedia);
+
     }
 }
