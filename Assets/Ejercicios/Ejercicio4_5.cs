0276046377b3171a276b2504d052961e73ae7057
Carlosa-1302 <carlosandresinfo13@gmail.com>
2024-09-28 06:05:24 +0200

    fsdfs

--- Assets/Ejercicios/Ejercicio4_5.cs
+++ Assets/Ejercicios/Ejercicio4_5.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class Ejercicio4_5 : MonoBehaviour
 {
+    [SerializeField] float radio;
+    [SerializeField] float pi = 3.14f;
+    [SerializeField] float Base;
+    [SerializeField] float altura;
+    [SerializeField] float lado;
+    [SerializeField] float lado2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        float areaCirculo = calcularCirculo(5);
+        Debug.Log("el area del ciruclo es " + areaCirculo);
+
+
+
+
+
+
+        float cantidadMoneda = convertir(areaCirculo, "dolar");
+        Debug.Log(cantidadMoneda);
     }
 
     // Update is called once per frame
-    void Update()
+    public float calcularCirculo(float radio)
     {
-        
+        float resultadoCirculo = radio * radio * pi;
+        return resultadoCirculo;
+
+    }
+
+    public float convertir(float areaCirculo , string moneda)
+    {
+        if (moneda == "euro")
+        {
+            float conversorEuro = areaCirculo * 1.1f;
+            Debug.Log("el resultado del circulo " + areaCirculo + " son " + conversorEuro + " dolares");
+            return conversorEuro;
+        }
+        else if (moneda == "dolar")
+        {
+            float conversorDolar = areaCirculo / 1.1f;
+            Debug.Log("el resultado del circulo " + areaCirculo + " son " + conversorDolar + " euros");
+            return conversorDolar;
+        }
+        return 0;
+
+
     }
 }
