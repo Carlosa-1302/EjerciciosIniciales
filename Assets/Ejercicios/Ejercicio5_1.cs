6a88219cc17d838652785a4a7b2851d07b355402
Carlosa-1302 <carlosandresinfo13@gmail.com>
2024-09-29 02:54:49 +0200

    sdfsdf

--- Assets/Ejercicios/Ejercicio5_1.cs
+++ Assets/Ejercicios/Ejercicio5_1.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Ejercicio5_1 : MonoBehaviour
 {
+    [SerializeField]int vida1, vida2 ;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (vida1 == vida2)
+        {
+            Debug.Log("empate");
+        }
+        else if (vida1 >= vida2)
+        {
+            Debug.Log("gana el jugador 1");
+        }
+        else if (vida2 >= vida1)
+        {
+            Debug.Log("gana el jugador 2");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+
         
     }
 }
