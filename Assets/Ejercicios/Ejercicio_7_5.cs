2f2573041f1f40288f48a78310ef8a6c0bceefe8
Carlosa-1302 <carlosandresinfo13@gmail.com>
2024-09-29 07:42:23 +0200

    asdas

---
+++ Assets/Ejercicios/Ejercicio_7_5.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio_7_5 : MonoBehaviour
+{
+    [SerializeField] int valor;
+    int numero = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        while (numero <= valor)
+        {
+            Debug.Log("el numero actual es " + numero);
+            numero++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
