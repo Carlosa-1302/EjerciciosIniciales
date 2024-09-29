e90b46fcab87e2a3988cd0ee1c3dbdf49d0c9610
Carlosa-1302 <carlosandresinfo13@gmail.com>
2024-09-29 06:34:50 +0200

    DSA

---
+++ Assets/Ejercicios/Ejercicio6_2.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio6_2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        /* a)F
+         * b)T
+         * c)F
+         * d)F
+         * e)T
+         * f)F
+         */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
