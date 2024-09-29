606a09d852c41a9794dbbe79108395d31b1a0c83
Carlosa-1302 <carlosandresinfo13@gmail.com>
2024-09-29 04:41:01 +0200

    dsfsdf

--- Assets/Ejercicios/Ejercicio5_6.cs
+++ Assets/Ejercicios/Ejercicio5_6.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Ejercicio5_6 : MonoBehaviour
 {
+    [SerializeField] int nivel;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (nivel % 10 == 0)
+        {
+            Debug.Log("el pokemon a subido al nivel " + nivel + " nuevo ataque desbloqueado");
+        }
+        else
+        {
+            Debug.Log("el pokemon ha subido de nivel");
+        }
     }
 
     // Update is called once per frame
