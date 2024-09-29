c04eb37076fe16c7bb6971f1a4d480b453941008
Carlosa-1302 <carlosandresinfo13@gmail.com>
2024-09-29 07:17:39 +0200

    sad

---
+++ Assets/Ejercicios/Ejercicio7_2.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio7_2 : MonoBehaviour
+{
+    int numero = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int numero = 1; numero <=100; numero++)
+        {
+            Debug.Log("el numero actual es " + numero);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
