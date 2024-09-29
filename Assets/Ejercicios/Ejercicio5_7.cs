fa6dc36559f8a2db4ce796bc487d4f3a750555b8
Carlosa-1302 <carlosandresinfo13@gmail.com>
2024-09-29 05:04:56 +0200

    dsada

--- Assets/Ejercicios/Ejercicio5_7.cs
+++ Assets/Ejercicios/Ejercicio5_7.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class Ejercicio5_7 : MonoBehaviour
 {
+    int velocidad;
+    [SerializeField]int numero1, numero2, numero3;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if( numero1 > numero2 && numero1 > numero3 )
+        {
+            Debug.Log("atacara primero el jugador numero 1");
+
+        }
+        else if(numero2 > numero1 && numero2 > numero3)
+        {
+            Debug.Log("atacara primero el jugador numero 2");
+        }
+        else if (numero3 > numero1 && numero3 > numero2)
+        {
+            Debug.Log("atacara primero el jugador numero 3");
+
+        }
     }
 
     // Update is called once per frame
