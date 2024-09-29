7f7eabded8d624b0a8b445592a93bb8d7184a153
Carlosa-1302 <carlosandresinfo13@gmail.com>
2024-09-29 04:33:48 +0200

    dasd

--- Assets/Ejercicios/Ejercicio5_4.cs
+++ Assets/Ejercicios/Ejercicio5_4.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class Ejercicio5_4 : MonoBehaviour
 {
+
+    [SerializeField]int numero1, numero2;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (numero2 != 0)
+        {
+            int calculardivision = numero1 / numero2;
+            Debug.Log("el resultado es " + calculardivision);
+
+        }
+        else 
+        {
+            Debug.Log("el divisor no debe ser 0");
+
+        }
     }
 
     // Update is called once per frame
