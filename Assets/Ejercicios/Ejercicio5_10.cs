42379311a2a71fef57023d930c8c6a9da426260c
Carlosa-1302 <carlosandresinfo13@gmail.com>
2024-09-29 06:13:51 +0200

    dasd

--- Assets/Ejercicios/Ejercicio5_10.cs
+++ Assets/Ejercicios/Ejercicio5_10.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class Ejercicio5_10 : MonoBehaviour
 {
+    [SerializeField] int temperatura;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (temperatura <= 10 )
+        {
+            Debug.Log("el clima es frio");
+        }
+         if   ( temperatura  >= 10 && temperatura <= 20 )
+        {
+            Debug.Log("el clima esta nublado");
+        }
+         if (temperatura >= 20 &&  temperatura <= 30)
+        {
+            Debug.Log("el clima esta caluroso");
+        }
+        if (temperatura > 30)
+        {
+            Debug.Log("el clima esta tropical");
+        }
+
     }
 
     // Update is called once per frame
